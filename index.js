57bbf51ae2160a5cdd894664c800589f2adde5a4
Arcslash34 <113771795+Arcslash34@users.noreply.github.com>
2024-08-17 13:19:16 +0800

    added reload feature to solve database issue

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ passport.deserializeUser((id, done) => {
 
 // Define routes for each page
 app.get("/", (req, res) => {
-  res.render("homePage", { user: req.user });
+  res.render("homePage", { user: req.user, currentPath: req.path });
 });
 
 app.get('/articles', async (req, res) => {
@@ -131,7 +131,9 @@ app.get('/articles', async (req, res) => {
           }
 
           // Render the articles page with articles and comments
-          res.render('articlesPage', { cnaArticles, mhfArticles, comments });
+
+          res.render('articlesPage', {  user: req.user, currentPath: req.path, cnaArticles, mhfArticles, comments });
+
         });
       });
     });
@@ -141,9 +143,8 @@ app.get('/articles', async (req, res) => {
   }
 });
 
-
 app.get("/program", (req, res) => {
-  res.render("programPage");
+  res.render('programPage', { user: req.user, currentPath: req.path });
 });
 
 app.get("/programGames", (req, res) => {
@@ -151,12 +152,13 @@ app.get("/programGames", (req, res) => {
 });
 
 app.get("/about", (req, res) => {
-  res.render("aboutPage");
+  res.render("aboutPage",{ user: req.user, currentPath: req.path });
 });
 
 app.get("/contact", (req, res) => {
-  res.render("contactPage");
+  res.render("contactPage", {user: req.user, currentPath: req.path});
 });
+
 
 app.get("/login", (req, res) => {
   res.render("loginPage", { errors: req.flash("error") });
@@ -258,6 +260,29 @@ const storeMhfArticlesInDb = async (articles) => {
 app.use('/comments', commentRoutes);
 app.use('/chatbot', chatbotRoutes);
 app.use('/auth', authRoutes);
+
+
+// single articles
+app.get('/bipolar', (req, res) => {
+  res.render('articles/bipolar', {currentPath: req.path});
+});
+
+app.get('/schizo', (req, res) => {
+  res.render('articles/schizo', {currentPath: req.path});
+});
+
+app.get('/ptsd', (req, res) => {
+  res.render('articles/ptsd', {currentPath: req.path});
+});
+
+app.get('/anxiety', (req, res) => {
+  res.render('articles/anxiety', {currentPath: req.path});
+});
+
+app.get('/personality', (req, res) => {
+  res.render('articles/personality', {currentPath: req.path});
+});
+
 
 // Start the server
 const PORT = 3000;
